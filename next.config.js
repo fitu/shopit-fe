d82568179f637f092588f44b4a8f2cebeb55b315
Matteucci Victorio <victorio.matteucci@simplex.software>
2021-11-05 14:45:06 -0300

    [fix/colors]: fix colores issue

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+const path = require('path');
+
 module.exports = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [path.resolve(__dirname, 'styles')],
+        prependData: `
+            @use "colors.scss";
+            @use "variables.scss";
+        `,
+    },
+};
