638fdf0b93be40b97e634d8c28ef79d5aa257736
Matteucci Victorio <victorio.matteucci@simplex.software>
2021-11-26 21:01:44 -0300

    [feat/404]: add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from 'react';
+
+import styles from './404.module.scss';
+
+const NotFoundPage = (): ReactElement => {
+    return (
+        <div className={styles.container}>
+            Fooo!
+        </div>
+    )
+}
+
+export default NotFoundPage;
