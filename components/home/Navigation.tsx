094076dde7478d9fa332a441fbe207fc5f946cb7
Matteucci Victorio <victorio.matteucci@simplex.software>
2021-11-29 11:21:18 -0300

    [fix/eslint]: fix eslint rules

--- components/home/Navigation.tsx
+++ components/home/Navigation.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react';
+import React, { ReactElement } from 'react';
 import ListItem, { Item } from '../ui/ListItem';
 
 import styles from './navigation.module.scss';
--- components/products/search/SearchProducts.tsx
+++ components/products/search/SearchProducts.tsx
@@ -1,5 +1,5 @@
 import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
-import { ReactElement } from 'react';
+import React, { ReactElement } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import { useForm } from 'react-hook-form';
 import * as Yup from 'yup';
